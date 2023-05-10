b5811c48adf83072844111f4e2e342d1689df822
Rio Stokes <rio.stokes@gtech.co.uk>
2023-05-10 16:19:32 +0100

    Added Chat GPT Response

---
+++ script.js
@@ -0,0 +1,29 @@
+import { config } from "dotenv";
+import readline from "readline";
+
+config()
+
+import { Configuration, OpenAIApi } from "openai"
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.API_KEY
+}))
+
+
+const userInterface = new readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+    const res = await openai
+        .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role:"user", content: input}]
+    })
+    console.log(res.data.choices[0].message.content)
+    userInterface.prompt()
+})
+
+
